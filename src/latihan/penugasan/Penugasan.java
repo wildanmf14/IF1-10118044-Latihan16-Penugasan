435ae144f127bb7135704e9c9a434ef1110ecfca
Wildhevire <wildhevire404@gmail.com>
2019-09-26 18:27:13 +0000

    finished

---
+++ src/latihan/penugasan/Penugasan.java
@@ -0,0 +1,48 @@
+package latihan.penugasan;
+/*
+Nama        : Wildan Muhammad Fikri
+Kelas       : IF1
+NIM         : 10118044
+Deskripsi   : penugasan
+ */
+public class Penugasan {
+	public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 0;
+
+        System.out.println("Nilai a = " + a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("Nilai c = " + c + "\n");
+
+        c = a + b;
+        System.out.println("c = a + b = " + c);
+        c += a;
+        System.out.println("c += a = " + c);
+        c -= a;
+        System.out.println("c -= a = " + c);
+        c *= a;
+        System.out.println("c *= a = " + c);
+        c = 10;
+        c = 15;
+        c /= a;
+        System.out.println("c /= a = " + c);
+
+        a = 10;
+        c = 15;
+        c %= a;
+        System.out.println("c %= a = " + c);
+        c <<= 2;
+        System.out.println("c <<= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= 2 = " + c);
+        c >>= 2;
+        System.out.println("c >>= a = " + c);
+        c &= a;
+        System.out.println("c &= 2 = " + c);
+        c ^= a;
+        System.out.println("c ^= a = " + c);
+        c |= a;
+        System.out.println("c |= a = " + c);
+	}
+}
